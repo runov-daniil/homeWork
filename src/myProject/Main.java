c5dac7032869c9bbb8c33bd266d78008b85272f0
runov-daniil <runov-daniil@yandex.ru>
2016-12-11 17:31:46 +0300

    Создан шаблон командного общения приложения с пользователем

--- src/myProject/Main.java
+++ src/myProject/Main.java
@@ -1,8 +1,29 @@
 package myProject;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int True = 0;
+        while(True == 0) {
+            System.out.println("Введите номер задачи:");
+            System.out.println("1: Переворот массива;");
+            System.out.println("0: Выход из программы.");
+            int cmd = scanner.nextInt();
+            switch (cmd){
+                case 1:
+                    reversMass();
+                    break;
+                case 0:
+                    True = 1;
+                    break;
+            }
+        }
+    }
+
+    private static void reversMass() {
+        System.out.println("Модуль в разработке!!!");
     }
 }
