3d8b192665903b415a50fcec1890b3fe2401ad33
runov-daniil <runov-daniil@yandex.ru>
2016-12-11 17:49:50 +0300

    Исправлен баг, добавлено исключение введения несуществующей команды. Релизовано заполнение массива

--- src/myProject/Main.java
+++ src/myProject/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int True = 0;
         while(True == 0) {
-            System.out.println("Введите номер задачи:");
+            System.out.println("Введите номер команды:");
             System.out.println("1: Переворот массива;");
             System.out.println("0: Выход из программы.");
             int cmd = scanner.nextInt();
@@ -17,13 +17,26 @@ public class Main {
                     reversMass();
                     break;
                 case 0:
+                    System.out.println("Работа программы завершена");
                     True = 1;
+                    break;
+                default:
+                    System.out.println("Такой команды не существует!!!");
                     break;
             }
         }
     }
 
     private static void reversMass() {
-        System.out.println("Модуль в разработке!!!");
+        Scanner scanner = new Scanner(System.in);
+        int[] a = new int[5];
+        for(int i = 0; i < 5; i++){
+            System.out.println("a[" + i + "] = ");
+            a[i] = scanner.nextInt();
+        }
+        System.out.println("Исходный массив");
+        for(int i = 0; i < 5; i++){
+            System.out.print(a[i] + "\t");
+        }
     }
 }
